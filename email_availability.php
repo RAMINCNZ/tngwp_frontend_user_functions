3a03543a0b3d627940c2ab02ae91ae524314469f
Nate Jacobs <nate@natejacobs.org>
2013-01-02 23:19:58 -0700

    Start fresh

--- email_availability.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once('../../../wp-load.php');
-	$useremail = $_POST['user_email'];
-    if ( email_exists( $useremail ) )
-		echo "no";
-    else
-		echo "yes";
-?>
--- user_availability.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('../../../wp-load.php');
-//	$_POST['userlogin'] = "HeatherF";
-    $userlogin = $_POST['userlogin'];
-    if ( username_exists( $userlogin ) )
-		echo "no";
-    else
-		echo "yes";
-?>
