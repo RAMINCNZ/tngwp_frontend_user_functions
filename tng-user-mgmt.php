4310fc6037094056a6ca38f539230d9434e9ef14
Nate Jacobs <nate@natejacobs.org>
2013-01-04 14:51:26 -0700

    Create plugin class to start the plugin rolling

---
+++ tng-user-mgmt.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ *	Plugin Name:   TNG User Management
+ *  Plugin URI:    http://heatherfeuer.github.com/TNG_User_Meta/
+ *	Description:   Manage your TNG and WordPress users all from WordPress. The plugin provides shortcodes for custom registration and a front-end user profile page. 
+ *	Version: 	   2.0
+ *	Date:		   1/4/13
+ *	Author:        Heather Feuerhelm, Nate Jacobs <natejacobs.org>
+ *  Author URI:    http://uniquelyyours.blogdns.com
+ */
+ 
+TNGUserMgmt::init(); 
+
+/** 
+*	TNG User Mgmt
+*
+*	This class sets up all the required files and constants for the plugin.
+*
+*	@author		Nate Jacobs
+*	@date		1/4/13
+*	@since		2.0
+*/
+class TNGUserMgmt
+{
+	/** 
+	*	Initialize
+	*
+	*	Hook into WordPress and prepare all the methods as necessary.
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/4/13
+	*	@since		2.0
+	*
+	*	@param		
+	*/
+	public static function init()
+	{
+		add_action( 'plugins_loaded', array( __CLASS__, 'constants' ), 1 );
+		add_action( 'plugins_loaded', array( __CLASS__, 'includes' ), 2 );
+		add_action( 'plugins_loaded', array( __CLASS__, 'admin' ), 3 );
+		add_action( 'init', array( __CLASS__, 'localization' ) );
+		
+		register_activation_hook( __FILE__, array( __CLASS__, 'activation' ) );
+		register_deactivation_hook( __FILE__, array( __CLASS__, 'deletion' ) );
+	}
+
+	/** 
+ 	*	Localization
+ 	*
+ 	*	Add support for localization
+ 	*
+ 	*	@author		Nate Jacobs
+ 	*	@date		1/4/13
+ 	*	@since		2.0
+ 	*
+ 	*	@param		
+ 	*/
+ 	public function localization() 
+ 	{
+  		load_plugin_textdomain( 'tng-user-mgmt', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
+	}
+
+	/**
+	 *	Plugin Constants
+	 *
+	 *	Constants used throughout plugin are defined for later use.
+	 *
+	 *	@author		Nate Jacobs
+	 *	@date		1/4/13
+	 *	@since 		2.0
+	 *
+	 *	@param		null
+	 */	
+	public function constants() 
+	{
+		define( 'TNGUSERMGMT_VERSION', '2.0' );
+		define( 'TNGUSERMGMT_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+		define( 'TNGUSERMGMT_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+		define( 'TNGUSERMGMT_INCLUDES', TNGUSERMGMT_DIR.trailingslashit( 'inc' ) );
+		define( 'TNGUSERMGMT_ADMIN', TNGUSERMGMT_DIR.trailingslashit( 'admin' ) );
+	}
+	
+	/**
+	 *	Include Files
+	 *
+	 *	Lists the files used for plugin actions in the includes folder.
+	 *	They are stored in the inc folder. These files are able to be accessed from both front and back end of site.
+	 *
+	 *	@author		Nate Jacobs
+	 *	@date		1/4/13
+	 *	@since 		2.0
+	 *
+	 *	@param		null
+	 */
+	public function includes()
+	{
+		require_once( TNGUSERMGMT_INCLUDES . 'class-registration.php' );
+		require_once( TNGUSERMGMT_INCLUDES . 'class-utilities.php' );
+		require_once( TNGUSERMGMT_INCLUDES . 'class-profile.php' );
+	}
+	
+	/**
+	 *	Admin Files
+	 *
+	 *	Lists the files used for plugin actions in the admin dashboard. 
+	 *	They are stored in the admin folder. These files are only able to accessed from the back end of the site
+	 *
+	 *	@author		Nate Jacobs
+	 *	@date		1/4/13
+	 *	@since 		2.0
+	 *
+     *	@param		null
+	 */
+	public function admin()
+	{
+		if ( is_admin() ) 
+		{
+			require_once( TNGUSERMGMT_ADMIN . 'class-settings.php' );
+		}
+	}
+	
+	/** 
+	*	Activation
+	*
+	*	Runs the method when the plugin is activated.
+	*	Verifies the TNG plugin is installed and activated
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/4/13
+	*	@since		2.0
+	*
+	*	@param		null
+	*/
+	public function activation()
+	{
+		
+	}
+	
+	/** 
+	*	Deletion
+	*
+	*	Runs the method when the plugin is deleted.
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/4/13
+	*	@since		2.0
+	*
+	*	@param	null	
+	*/
+	public function deletion()
+	{
+		// remove all settings
+	}
+}
