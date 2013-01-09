1694c7773a69e4e1012167cf4581a8d92b099386
Nate Jacobs <nate@natejacobs.org>
2013-01-08 23:47:05 -0700

    Add users class
    Stubbed out basic functions

---
+++ inc/class-users.php
@@ -0,0 +1,77 @@
+<?php
+
+/** 
+*	TNG Users
+*
+*	
+*
+*	@author		Nate Jacobs
+*	@date		1/8/13
+*	@since		1.0
+*/
+class TNGUsers
+{
+	/** 
+	*	Construct Method
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/8/13
+	*	@since		1.0
+	*
+	*	@param		
+	*/
+	public function __construct()
+	{
+		
+	}
+
+	/** 
+	*	Create New TNG User
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/8/13
+	*	@since		1.0
+	*
+	*	@param		
+	*/
+	public function create_tng_user()
+	{
+		
+	}
+	
+	/** 
+	*	Update TNG User
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/8/13
+	*	@since		1.0
+	*
+	*	@param		
+	*/
+	public function update_tng_user()
+	{
+		
+	}
+	
+	/** 
+	*	Delete TNG User
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/8/13
+	*	@since		1.0
+	*
+	*	@param		
+	*/
+	public function delete_tng_user()
+	{
+		
+	}
+}
