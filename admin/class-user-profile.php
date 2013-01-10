a14ffe7f6e558076769208b041d7c05ed0675806
Nate Jacobs <nate@natejacobs.org>
2013-01-09 22:36:32 -0700

    New class for managing the permissions on user profile page

---
+++ admin/class-user-profile.php
@@ -0,0 +1,45 @@
+<?php
+
+/** 
+*	User Profile
+*
+*	This class is used to add extra fields to the user profile screen.
+*
+*	@author		Nate Jacobs
+*	@date		1/9/13
+*	@since		2.0
+*/
+class TNGUserProfile
+{
+	/** 
+	*	Initialize
+	*
+	*	Hook into WordPress and prepare all the methods as necessary.
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/9/13
+	*	@since		2.0
+	*
+	*	@param		
+	*/
+	public function init()
+	{
+		
+	}
+
+	/** 
+	*	Add TNG User Permissions
+	*
+	*	
+	*
+	*	@author		Nate Jacobs
+	*	@date		1/9/13
+	*	@since		2.0
+	*
+	*	@param		
+	*/
+	public function add_user_permissions()
+	{
+		// only available to manage_options
+	}
+}
